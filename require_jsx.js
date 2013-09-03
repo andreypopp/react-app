2e4a041dba986c91239d9da1f3f255d1c4201eb2
Andrey Popp <8mayday@gmail.com>
2013-09-03 22:21:54 +0400

    check if we already have .jsx require handler

--- require_jsx.js
+++ require_jsx.js
@@ -1,8 +1,11 @@
 var fs          = require('fs');
 var reactTools  = require('react-tools');
 
-require.extensions['.jsx'] = function(module, filename) {
-  var src = fs.readFileSync(filename, {encoding: 'utf-8'});
-  var compiled = reactTools.transform(src);
-  return module._compile(compiled, false);
+if (!require.extensions['.jsx']) {
+
+  require.extensions['.jsx'] = function(module, filename) {
+    var src = fs.readFileSync(filename, {encoding: 'utf-8'});
+    var compiled = reactTools.transform(src);
+    return module._compile(compiled, false);
+  };
 }
