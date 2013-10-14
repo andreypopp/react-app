dadbecba329cb8fca9001924a55d16ef53381af9
Andrey Popp <8mayday@gmail.com>
2013-10-15 01:11:45 +0400

    getActiveApp

--- browser.js
+++ browser.js
@@ -1,16 +1,7 @@
 "use strict";
 
-var getID = require('react-tools/build/modules/getReactRootElementInContainer'),
-    ReactMount = require('react-tools/build/modules/ReactMount');
-
-function currentApp(doc) {
-  var id = getID(doc || document);
-  id = id && ReactMount.getID(id);
-  return ReactMount._instanceByReactRootID[id].props.app;
-}
-
 module.exports = {
-  currentApp: currentApp,
+  getActiveApp: function() { return require('' + 'react-app/runtime/browser').app; },
   createApp: require('./app'),
   createPage: require('./page').createPage
 };
