2aa4b19823758703f94335ae81c01a09dd4a331c
Andrey Popp <8mayday@gmail.com>
2013-11-21 12:28:28 +0400

    browser: createApp => createController

--- browser.js
+++ browser.js
@@ -1,5 +1,4 @@
 "use strict";
-var createApp = require('./controller');
-module.exports = createApp;
-module.exports.createApp = createApp;
-
+var createController = require('./controller');
+module.exports = createController
+module.exports.createController = createController
