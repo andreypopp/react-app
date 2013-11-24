03375572555757ab195a57707c37f7f36724ed8c
Andrey Popp <8mayday@gmail.com>
2013-11-25 01:22:15 +0400

    Fix controller first data fetch

--- controller.js
+++ controller.js
@@ -1,60 +1,88 @@
 "use strict";
 
-var basicController       = require('react-app-controller');
-var cloneDeep             = require('lodash.clonedeep');
-var assign                = require('lodash.assign');
+var React           = require('react-tools/build/modules/React');
+var invariant       = require('react-tools/build/modules/invariant');
+var Base            = require('react-app-controller');
+var request         = require('react-app-controller/request');
+var cloneDeep       = require('lodash.clonedeep');
+var assign          = require('lodash.assign');
 
-function createController(routes, opts) {
-  opts = opts || {};
-  opts.renderComponent = renderComponent;
-  opts.renderComponentToString = renderComponentToString;
-  return basicController(routes, opts);
-}
+var ControllerInterface = assign({}, Base.ControllerInterface, {
 
-function wrapComponentWithData(component, data) {
-  var getInitialState = component.getInitialState;
-  component.getInitialState = function() {
-    return getInitialState ?
-      assign(getInitialState(), cloneDeep(data)) :
-      cloneDeep(data);
+  /**
+   * Override request process so it first fetches the data needed for a page
+   * transition.
+   */
+  process: function(req, cb) {
+    req = request.normalizeRequest(req);
+
+    var page;
+    try {
+      page = this.createPageForRequest(req);
+    } catch(err) {
+      return cb(err);
+    }
+
+    var needData = typeof page.fetchData === 'function' && !req.data;
+
+    if (request.isEqual(this.state.request, req) && !needData)
+      return;
+
+    fetchDataForRequest(this, page, req, function(err, req) {
+      if (err) {
+        throw err;
+      }
+      this.setState({request: req, page: page});
+    }.bind(this));
   }
-  return component;
-}
+});
 
-/**
- * Bootstrap component with data it needs to fetch
- */
-function bootstrapComponent(component, request, cb) {
-  if (!component.fetchData)
-    return cb(null, component, undefined);
+var ControllerRenderingInterface = assign({}, Base.ControllerRenderingInterface, {
+  renderToString: function(req, cb) {
+    invariant(
+      typeof cb === 'function',
+      'provide callback as a last argument to renderToString(...)'
+    );
 
-  if (request.data)
-    return cb(null, wrapComponentWithData(component, request.data), request.data);
+    req = request.normalizeRequest(req);
 
-  component.fetchData(function(err, data) {
-    if (err) return cb(err);
+    var controller, page;
+    try {
+      controller = this({request: request.normalizeRequest(req)});
+      page = controller.createPageForRequest(req);
+    } catch(err) {
+      return cb(err);
+    }
 
-    cb(null, wrapComponentWithData(component, data), data);
-  });
-}
-
-function renderComponent(component, element, request, cb) {
-  bootstrapComponent(component, request, function(err, component) {
-    if (err) return cb(err);
-    basicController.renderComponent(component, element, request, cb);
-  });
-}
-
-function renderComponentToString(component, request, cb) {
-  bootstrapComponent(component, request, function(err, component, data) {
-    if (err) return cb(err);
-    basicController.renderComponentToString(component, request, function(err, rendered) {
-      if (err) return cb(err);
-      rendered.data = data;
-      rendered.title = component.getTitle ? component.getTitle() : undefined;
-      cb(null, rendered);
+    fetchDataForRequest(controller, page, req, function(err, req) {
+      try {
+        React.renderComponentToString(controller, function(markup) {
+          cb(null, {markup: markup, request: req});
+        });
+      } catch (err) {
+        console.log('oops', err);
+        cb(err);
+      }
     });
-  });
+  }
+});
+
+function fetchDataForRequest(controller, page, req, cb) {
+  if (req.data || typeof page.fetchData !== 'function') {
+    cb(null, req, page);
+  } else {
+    page.fetchData(req, function(err, data) {
+      if (err) return cb(err);
+      req.data = cloneDeep(data);
+      cb(null, req, page);
+    });
+  }
+}
+
+function createController(spec) {
+  return Base.createController(spec,
+    ControllerInterface,
+    ControllerRenderingInterface);
 }
 
 module.exports = createController;
